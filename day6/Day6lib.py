08c8517872ade894e4fd29a8db7aa72e115d41a1
Siva Kumar Grahadurai <narthana23@gmail.com>
2023-12-06 10:37:38 +0530

    Day 6 - skeleton

---
+++ day6/Day6lib.py
@@ -0,0 +1,35 @@
+from enum import IntEnum
+from os.path import dirname, join
+
+# class syntax
+
+InputFileList = [
+    "./input_sample_1.txt",
+    "./input_sample_2.txt",
+    "./input.txt",
+]
+
+class INPUT_LOC(IntEnum):
+    SAMPLE_INPUT_1 = 0
+    SAMPLE_INPUT_2 = 1
+    ORIGINAL_INPUT = 2
+
+class Day6lib():
+    def __init__( self ):
+        pass
+
+    def readFileContent( self, inputType ):
+        CurrentDir = dirname( __file__ )
+        InputFilePath = join( CurrentDir, InputFileList[inputType] )
+
+        try:
+            inputFile = open( InputFilePath, "r+" )
+        except Exception:
+            print( "Error: File does not appear to exist." )
+            return None
+        
+        lines = inputFile.readlines()
+        inputFile.close()
+        self.maxRow = len(lines)
+        self.maxColumn = len(lines[0]) - 1 
+        return (lines)
---
+++ day6/WinningCombination.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from Day6lib import *
+import re
+
+class WinningCombination(Day6lib):
+
+    def __init__(self):
+        pass
+    
+    def run(self):
+        self.main()
+
+    def main(self):
+        print("Hello! World")
+
+if __name__ == "__main__":
+    winningCombination = WinningCombination()
+    winningCombination.run()
