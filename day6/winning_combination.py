e7d83f836feb83a62a372a7cc0b04781e68c950d
Siva Kumar Grahadurai <narthana23@gmail.com>
2023-12-06 11:27:33 +0530

    Pylint fix - part 2

--- day6/winning_combination.py
+++ day6/winning_combination.py
@@ -1,16 +1,20 @@
+""" Puzzle 1 """
 #!/usr/bin/python3
-from Day6lib import *
-import re
+#from day6_lib import *
+#import re
 
-class WinningCombination(Day6lib):
+class WinningCombination(): #Day6lib):
+    """ Puzzle 1 solution """
 
     def __init__(self):
         pass
-    
+
     def run(self):
+        """ Run method """
         self.main()
 
     def main(self):
+        """ Main method """
         print("Hello! World")
 
 if __name__ == "__main__":
