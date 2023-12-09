f2ec0c21759e84ba4f0a2e5c308b315011956112
Siva Kumar G <sivagkumarcse@gmail.com>
2023-12-09 15:42:50 +0530

    Day 8 - Complete

--- day8/day8_lib.py
+++ day8/day8_lib.py
@@ -1,10 +1,13 @@
 """ Lib providing basic APIs """
+import re
 from enum import IntEnum
 from os.path import dirname, join
+from itertools import cycle
 
 InputFileList = [
     "./input_sample_1.txt",
     "./input_sample_2.txt",
+    "./input_sample_3.txt",
     "./input.txt",
 ]
 
@@ -12,7 +15,8 @@ class InputLoc(IntEnum):
     """ Enum to select input file """
     SAMPLE_INPUT_1 = 0
     SAMPLE_INPUT_2 = 1
-    ORIGINAL_INPUT = 2
+    SAMPLE_INPUT_3 = 2
+    ORIGINAL_INPUT = 3
 
 class Day8lib():
     """ Base class for Day 8 puzzle """
@@ -20,7 +24,8 @@ class Day8lib():
     max_column = 0
 
     def __init__( self ):
-        pass
+        self.input_steps = None
+        self.maps_network = {}
 
     def read_file_content( self, input_type ):
         """ Open input file and return its content """
@@ -34,3 +39,40 @@ class Day8lib():
         self.max_row = len(lines)
         self.max_column = len(lines[0]) - 1
         return lines
+
+    def convert_steps(self, step):
+        """ Convert input L|R to 0|1 """
+        if step == 'L':
+            return 0
+        if step == 'R':
+            return 1
+        return None
+
+    def parse_input_to_maps_network(self, file_content):
+        """ Generate input dict and steps from the file content """
+        steps_logged = False
+
+        for line in file_content:
+            # Read first line which has steps
+            if steps_logged is False:
+                self.input_steps = [ self.convert_steps(steps) for steps in line if steps != '\n' ]
+                steps_logged = True
+                continue
+
+            if len(line) != 1: # Ignore line with only '\n'
+                re_match = re.findall(r'(\w+)', line)
+                self.maps_network[re_match[0]] = [re_match[1], re_match[2]]
+
+    def count_steps(self, current_loc, dest_wildcard=False):
+        """ Count steps from current_loc to dest """
+        steps = 0
+        for direction in cycle(self.input_steps):
+            if dest_wildcard is True:
+                if current_loc[-1] == 'Z':
+                    break
+            else:
+                if current_loc == 'ZZZ':
+                    break
+            steps += 1
+            current_loc = self.maps_network[current_loc][direction]
+        return steps
--- day8/steps_calculator.py
+++ day8/steps_calculator.py
@@ -1,6 +1,7 @@
 """ Puzzle 1 """
 #!/usr/bin/python3
-import re
+import sys
+from math import lcm
 from day8_lib import Day8lib
 from day8_lib import InputLoc
 
@@ -8,23 +9,40 @@ class StepsCalculator(Day8lib):
     """ Puzzle 1 solution """
     sum_of_prediction = 0
 
-    def __init__(self):
-        pass
-
     def run(self):
         """ Run method """
         self.main()
 
     def main(self):
         """ Main method """
+        sys.setrecursionlimit(25000)
+        ghost_total_steps = []
         total_steps = 0
+        total_steps_p2 = 1 # For LCM calculation
         # Use either SAMPLE_INPUT_1 or ORIGINAL_INPUT
-        content = self.read_file_content(InputLoc.SAMPLE_INPUT_1)
+        content = self.read_file_content(InputLoc.ORIGINAL_INPUT)
+        self.parse_input_to_maps_network(content)
 
-        print(content)
-        print( f"Total steps to reach from AAA->ZZZ is {total_steps}" )
-        return total_steps
+        try:
+            total_steps = self.count_steps("AAA")
+        except KeyError:
+            print("Unsupported input file for puzzle 1")
+            total_steps = 0
 
+        print( f"Solution 1: Total steps to reach from AAA->ZZZ is {total_steps}" )
+
+        for key in self.maps_network:
+            if 'A' == key[-1]:
+                ghost_total_steps.append(self.count_steps(key, dest_wildcard=True) )
+        try:
+            for step in ghost_total_steps:
+                total_steps_p2 = lcm(step, total_steps_p2)
+        except KeyError:
+            print("Unsupported input file for puzzle 1")
+            total_steps_p2 = 0
+
+        print( f"Solution 2: Total steps to reach from ..A->..Z is {total_steps_p2}" )
+        return total_steps, total_steps_p2
 
 if __name__ == "__main__":
     stepsCalculator = StepsCalculator()
